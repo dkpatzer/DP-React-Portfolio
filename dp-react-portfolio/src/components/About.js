08a5d625fb8983e184d6995d2aa5da3ba66924d0
“firstname <38668042+dkpatzer@users.noreply.github.com>
2023-07-24 00:31:55 -0500

    added resume file

--- dp-react-portfolio/src/components/About.js
+++ dp-react-portfolio/src/components/About.js
@@ -7,7 +7,7 @@ export default function About() {
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Welcome to my portfolio. I'm David Patzer, M.D., a full-stack web developer
-            <br className="hidden lg:inline-block" />I'm a physician and full-stack web developer with a passion for building
+            <br className="hidden lg:inline-block" />I'm a physician and full-stack web developer with a passion for making your dreams come alive!
             apps.
           </h1>
           <p className="mb-8 leading-relaxed">
