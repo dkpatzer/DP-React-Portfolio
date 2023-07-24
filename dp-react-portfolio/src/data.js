e44cc302591657e8ddf459d0b367bfa15b7e36ef
“firstname <38668042+dkpatzer@users.noreply.github.com>
2023-07-23 21:29:31 -0500

    adding data to files

---
+++ dp-react-portfolio/src/data.js
@@ -0,0 +1,34 @@
+export const projects = [
+    {
+      title: "Five Day Weather App",
+      subtitle: "Server Side API's Project",
+      description:
+        "This project uses the OpenWeather API to retrieve weather data for cities. The app runs in the browser and features dynamically updated HTML and CSS powered by jQuery.",
+      image: "./images/5-day weather app.png",
+      link: "https://github.com/dkpatzer/Five-Day-Weather-App/",
+    },
+    {
+      title: "Knowledge Is Power Quiz App",
+      subtitle: "JAKGD Project",
+      description:
+        "This project uses the OpenTrivia API to retrieve trivia questions. The app runs in the browser and features dynamically updated HTML and CSS powered by jQuery.",
+      image: "./images/Patch Tracker.png",
+      link: "https://github.com/JAKGD/knowledge-is-power",
+    },
+    {
+      title: "Food Friendz App",
+      subtitle: "Food Friendz Project",
+      description:
+        "Food Friendz let us organize a food fair on your street.",
+      image: "./images/Food Friendz Logo.png",
+      link: "https://github.com/orgs/food-friendz/repositories",
+    },
+    {
+      title: "Epic Todo App",
+      subtitle: "React Hooks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-4.gif",
+      link: "https://pythonbootcamp.com",
+    },
+  ];
