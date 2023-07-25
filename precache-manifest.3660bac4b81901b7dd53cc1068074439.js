a52127d4d62c5d199958e05185b5598ee838d2e0
“firstname <38668042+dkpatzer@users.noreply.github.com>
2023-07-25 02:00:49 -0500

    Updates

---
+++ precache-manifest.3660bac4b81901b7dd53cc1068074439.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "71e354fca6317a7ada4f",
+    "url": "/your-repository-name/static/css/main.f220d198.chunk.css"
+  },
+  {
+    "revision": "71e354fca6317a7ada4f",
+    "url": "/your-repository-name/static/js/main.71e354fc.chunk.js"
+  },
+  {
+    "revision": "20e15a616ba784fadde6",
+    "url": "/your-repository-name/static/js/1.20e15a61.chunk.js"
+  },
+  {
+    "revision": "65ddd05ec64f7894dc5e",
+    "url": "/your-repository-name/static/js/runtime~main.65ddd05e.js"
+  },
+  {
+    "revision": "7571ea13179d06d922f912f64d14abc6",
+    "url": "/your-repository-name/static/media/2021 Twitter logo - blue.7571ea13.png"
+  },
+  {
+    "revision": "24b60593fd768dac8e4f66a502bffa89",
+    "url": "/your-repository-name/index.html"
+  }
+];
